38b4c26309a66005446689125d6e59d3892b2ad9
lauren <ljdutton2@icloud.com>
2019-09-20 18:57:53 -0700

    init

---
+++ dog.py
@@ -0,0 +1,8 @@
+class Dog:
+    def__init__(self,name,breed):
+    self.name = name
+    self.breed = breed
+    def bark(self):
+        print("Woof!!")
+my_dog = Dog("Rex", "Superdog")
+print(my_dog.bark()
