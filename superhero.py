23b161c56427db4f7ed4c72542335df505808fc4
lauren <ljdutton2@icloud.com>
2019-09-26 21:53:46 -0700

    [add] Arena Class

--- superhero.py
+++ superhero.py
@@ -1,6 +1,6 @@
 import random
 #Lauren todo fix pytest
-#super function 
+#super function
 
 class Ability:
     def __init__(self, name, max_damage):
@@ -31,14 +31,14 @@ class Hero:
 #I dont understand how/why we can make self variables if its not a paramater in init
             self.deaths = 0
             self.kills = 0
+    def add_weapon(self, weapon):
+        self.abilities.append(weapon)
 
     def add_kills(self,num_kills):
         self.kills += num_kills
 
     def add_deaths(self, num_deaths):
         self.kills += num_deaths
-
-
 
     def attack(self):
         total_damage = 0
@@ -50,7 +50,6 @@ class Hero:
 
     def add_ability(self, ability):
         self.abilities.append(ability)
-
 
     def add_armor(self, armor):
         self.armors.append(armor)
@@ -96,25 +95,113 @@ class Weapon(Ability):
         self.name = name
         self.max_damage = max_damage
         #super() function
-    def attack(self):
+    def attack(self,other_team):
         weapon_attack = self.max_damage // 2
         return (random.randint(weapon_attack, self.max_damage))
 
 
 class Team:
-    def __init__(self,name,):
+    def __init__(self,name):
         self.name = name
+        self.heroes = []
 
     def add_hero(self,hero):
-        hero_list = self.hero = []
-        hero_list.append(hero)
+        self.heroes.append(hero)
 
     def remove_hero(self,hero):
-        return hero_list.remove(hero)
+        return self.heroes.remove(hero)
 
     def viewallheroes(self):
-        for hero in self.hero:
+        for hero in self.heroes:
             print(hero.name)
+
+    def revive_heroes(self, health=100):
+         for hero in self.heroes:
+             hero.current_health = health
+
+    def stats(self):
+        for hero in self.heroes:
+            print(hero.name + ': \n Kills:' + hero.kills + " Deaths: " + hero.deaths)
+
+	def attack(self, other_team):
+		hero_attackers = []
+		opponent_attackers = []
+		for hero in self.heroes:
+			while len(hero_attackers) > 0:
+				if living_hero.isalive() == True:
+				hero_attackers.append(living_hero)
+				hero_fighter = random.choice(hero_attackers)
+		for hero in opponent.heroes:
+			while len(opponent_attackers) > 0:
+				if opponent.isalive() == True:
+				opponent_attackers.append(opponent)
+				opponent_fighter = random.choice(opponent_attackers)
+
+		hero_fighter.fight(opponent.fighter)
+
+class Arena:
+    def __init__(self):
+		self.team_one = []
+		self.team_two = []
+
+
+    def create_ability(self):
+        ability_1 = input("Choose an ability for your hero")
+        ability_max = input(f"What is the max damage of {ability_1}: ")
+        user_ability = Ability(ability_1, ability_max)
+        return user_ability
+
+    def create_weapon(self):
+        weapon_1 = input("Choose a weapon for your hero")
+        weapon_max = input(f"What is the max damage of {weapon_1}: ")
+        user_weapon = Weapon(weapon_1, weapon_max)
+        return user_weapon
+
+    def create_armor(self):
+        armor_1 = input("Choose an armor to protect your hero")
+        armor_max = input(f"What is the max protection of the {armor_1}: ")
+        user_armor = Armor(armor_1, armor_max)
+        return user_armor
+
+    def create_hero(self):
+        hero_name = input("Choose a name for your hero!")
+        hero = Hero(hero_name)
+        hero.abilities = hero.append(create_ability())
+        hero.armors = hero.append(create_armor())
+        hero.abilities = hero.append(create_weapon())
+        return hero
+
+
+    def build_team_one(self):
+       team_1_name = input("Choose a name for Team 1")
+       first_team = Team(team_1_name)
+       team_1_heroes = int(input(f"How many heroes are on {}? "))
+       for _ in team_1_heroes:
+           hero = self.create_hero()
+           first_team.add_hero(hero)
+      	return first_team.append(self.team_one)
+
+    def build_team_two(self):
+		team_2_name = input("Choose a name for Team 2")
+		second_team = Team(team_2_name)
+		team_2_heroes = int(input(f"How many heroes are on {}? ""))
+		for _ in team_2_heroes:
+			hero = self.create_hero()
+			second_team.add_hero(hero)
+		return second_team.append(self.team_two)
+
+
+    def team_battle(self):
+		print("Let the battle begin!")
+		Hero.fight(self.team_one)(self.team_two)))
+
+	def show_stats(self):
+		team_1_results = 0
+		team_2_results = 0
+
+
+
+
 
 
 
@@ -124,4 +211,4 @@ class Team:
 if __name__ == '__main__':
     knife = Weapon("knife", 30)
     print(knife.name)
-    print(knife.attack())
+    # print(knife.attack())
