aeb0ff5f98af8dea34cd7f120ebcf195a8b9a16e
lauren <ljdutton2@icloud.com>
2019-09-26 22:57:57 -0700

    [fix] indentation error

--- superhero.py
+++ superhero.py
@@ -121,34 +121,35 @@ class Team:
             print(hero.name)
 
     def revive_heroes(self, health=100):
-         for hero in self.heroes:
-             hero.current_health = health
+        for hero in self.heroes:
+            hero.current_health = health
 
     def stats(self):
         for hero in self.heroes:
             print(hero.name + ': \n Kills:' + hero.kills + " Deaths: " + hero.deaths)
 
-	def attack(self, other_team):
-		hero_attackers = []
-		opponent_attackers = []
-		for hero in self.heroes:
-			while len(hero_attackers) > 0:
-				if living_hero.isalive() == True:
-					hero_attackers.append(living_hero)
-					hero_fighter = random.choice(hero_attackers)
-		for hero in opponent.heroes:
-			while len(opponent_attackers) > 0:
-				if opponent.isalive() == True:
-					opponent_attackers.append(opponent)
-opponent_fighter = random.choice(opponent_attackers)
+    def attack(self, other_team):
+        hero_attackers = []
+        opponent_attackers = []
+        for hero in self.heroes:
+            while len(hero_attackers) > 0:
+                if living_hero.isalive() == True:
+                    hero_attackers.append(living_hero)
+                    hero_fighter = random.choice(hero_attackers)
 
-hero_fighter.fight(opponent.fighter)
+        for hero in opponent.heroes:
+            while len(opponent_attackers) > 0:
+                if opponent.isalive() == True:
+                    opponent_attackers.append(opponent)
+                    opponent_fighter = random.choice(opponent_attackers)
+
+        hero_fighter.fight(opponent.fighter)
 
 
 class Arena:
     def __init__(self):
-		self.team_one = []
-		self.team_two = []
+        self.team_one = self.build_team_one()
+        self.team_two = self.build_team_two()
 
     def create_ability(self):
         ability_1 = input("Choose an ability for your hero")
@@ -177,49 +178,42 @@ class Arena:
         return hero
 
     def build_team_one(self):
-       team_1_name = input("Choose a name for Team 1")
-       first_team = Team(team_1_name)
-       team_1_heroes = int(input(f"How many heroes are on {}? "))
-       for _ in team_1_heroes:
-           hero = self.create_hero()
-           first_team.add_hero(hero)
-      	return first_team.append(self.team_one)
+        team_1_name = input("Choose a name for Team 1")
+        first_team = Team(team_1_name)
+        team_1_heroes = int(input(f"How many heroes are on {team_1_name}? "))
+        for _ in team_1_heroes:
+            hero = self.create_hero()
+            first_team.add_hero(hero)
+        return first_team
 
     def build_team_two(self):
-		team_2_name = input("Choose a name for Team 2")
-		second_team = Team(team_2_name)
-		team_2_heroes = int(input(f"How many heroes are on {}? ""))
-		for _ in team_2_heroes:
-			hero = self.create_hero()
-			second_team.add_hero(hero)
-		return second_team.append(self.team_two)
+        team_2_name = input("Choose a name for Team 2")
+        second_team = Team(team_2_name)
+        team_2_heroes = int(input(f"How many heroes are on {team_2_name}? "))
+        for _ in team_2_heroes:
+            hero = self.create_hero()
+            second_team.add_hero(hero)
+        return second_team
 
 
     def team_battle(self):
-		print("Let the battle begin!")
-		Hero.fight(self.team_one)(self.team_two)))
+        print("Let the battle begin!")
+        Hero.fight(self.team_one, self.team_two)
 
-	def show_stats(self):
-		# need to add winning team
-		# add k/d
-		# show surviving heroes
-		print("The results are: ")
-		team_1_result = self.team_one.stats()
-		team_2_results = self.team_two.stats()
-		print(team_1_result)
-		print(team_2_results)
-
-
-
-
-
-
-
+    def show_stats(self):
+        # need to add winning team
+        # add k/d
+        # show surviving heroes
+        print("The results are: ")
+        team_1_result = self.team_one.stats()
+        team_2_results = self.team_two.stats()
+        print(team_1_result)
+        print(team_2_results)
 
 
 
 if __name__ == '__main__':
- arena = Arena()
+    arena = Arena()
     arena.build_team_one()
     arena.build_team_two()
     arena.team_battle()
